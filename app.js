c47f5ea76cb7d7b6cc8582565be131fef60880a5
kitjust <kitjust410@gmail.com>
2024-02-07 23:25:18 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const $gifArea = $("#gif-area");
+const $searchInput = $("#search");
+
+/* use ajax result to add a gif */
+
+function addGif(res) {
+  let numResults = res.data.length;
+  if (numResults) {
+    let randomIdx = Math.floor(Math.random() * numResults);
+    $gifArea.append(
+      $("<div>", { class: "col-md-4 col-12 mb-4" }).append(
+        $("<img>", {
+          src: res.data[randomIdx].images.original.url,
+          class: "w-100"
+        })
+      )
+    );
+  }
+}
+
+/* handle form submission: clear search box & make ajax call */
+
+$("form").on("submit", async function(evt) {
+  evt.preventDefault();
+
+  let searchTerm = $searchInput.val();
+  $searchInput.val("");
+
+  const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      q: searchTerm,
+      api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+    }
+  });
+  addGif(response.data);
+});
+
+/* remove gif */
+
+$("#remove").on("click", function() {
+  $gifArea.empty();
+});
